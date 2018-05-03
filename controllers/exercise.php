303245b733ab4d30ee87e3c68da15fc85e4448d4
Erin McMackin <mcmackinerin@gmail.com>
2018-05-03 16:09:27 -0400

    index php working

--- controllers/exercise.php
+++ controllers/exercise.php
@@ -1,13 +1,17 @@
 <?php
 header('Content-Type: application/json');
 include_once __DIR__ . '/../models/exercise.php';
+
 if($_REQUEST['action'] === 'index'){
     echo json_encode(Exercises::find());
 } else if ($_REQUEST['action'] === 'post'){
     $requestBody = file_get_contents('php://input');
     $body = json_decode($requestBody);
+
     $newExercise = new Exercise(null, $body->title, $body->intensity, $body->focus, $body->description, $body->image);
-    $allExercises = Exercise::create($newExercise);
+
+    $allExercises = Exercises::create($newExercise);
+
     echo json_encode($allExercises);
 } else if ($_REQUEST['action'] === 'delete'){
     $allExercises = Exercises::delete($_REQUEST['id']);
@@ -17,6 +21,7 @@ if($_REQUEST['action'] === 'index'){
     $body = json_decode($requestBody);
     $updatedExercise = new Exercise(null, $body->title, $body->intensity, $body->focus, $body->description, $body->image);
     $allExercises = Exercises::update($_REQUEST['id'], $updatedExercise);
+    
     echo json_encode($allExercises);
 }
 ?>
--- models/exercise.php
+++ models/exercise.php
@@ -1,80 +1,73 @@
 <?php
-
-$dbconn = null;
-if(getenv('DATABASE_URL')){
-    $connectionConfig = parse_url(getenv('DATABASE_URL'));
-    $host = $connectionConfig['host'];
-    $user = $connectionConfig['user'];
-    $password = $connectionConfig['pass'];
-    $port = $connectionConfig['port'];
-    $dbname = trim($connectionConfig['path'],'/');
-    $dbconn = pg_connect(
-        "host=".$host." ".
-        "user=".$user." ".
-        "password=".$password." ".
-        "port=".$port." ".
-        "dbname=".$dbname
-    );
-} else {
-    $dbconn = pg_connect("host=localhost dbname=fit_builder");
-}
+include_once __DIR__ . '/../database/db.php';
 
 class Exercise {
-    public $id;
-    public $title;
-    public $intensity;
-    public $focus;
-    public $description;
-    public $image;
-    public function __construct($id, $title, $intensity, $focus, $description, $image) {
-        $this->id = $id;
-        $this->title = $title;
-        $this->intensity = $intensity;
-        $this->focus = $focus;
-        $this->description = $description;
-        $this->image = $image;
-    }
+  public $id;
+  public $title;
+  public $intensity;
+  public $focus;
+  public $description;
+  public $image;
+  public function __construct($id, $title, $intensity, $focus, $description, $image) {
+    $this->id = $id;
+    $this->title = $title;
+    $this->intensity = $intensity;
+    $this->focus = $focus;
+    $this->description = $description;
+    $this->image = $image;
+  }
 }
 
-Class Exercises {
-    public $title;
-    static public function create($title, $intensity, $focus, $description){
-        //connect just once, not for every create/find
-        //put these in a properties file that gets ignored from git
-        $servername = 'localhost';
-        $username = 'root';
-        $password = 'root';
-        $dbname = 'fit_builder';
-
-        $mysql_connection = new mysqli($servername, $username, $password, $dbname);
-
-        if($mysql_connection->connect_error){
-            die('Connection Failed: ' . $mysql_connection->connect_error);
-        } else {
-            $sql = "INSERT INTO exercises (title, intensity, focus, description, image) VALUES ('".$title."', '".$intensity."', '".$focus."', '".$description."', '".$image."');";
-            $mysql_connection->query($sql);
-        }
-        $mysql_connection->close();
+class Exercises {
+  static public function find(){
+     //connect just once, not for every create/find
+    // $servername = 'localhost';
+    // $username = 'root';
+    // $password = 'root';
+    // $dbname = 'fit_builder';
+    //
+    // $mysql_connection = new mysqli($servername, $username, $password, $dbname);
+    //
+    // if($mysql_connection->connect_error){
+    //   $mysql_connection->close();
+    //   die('Connection Failed: ' . $mysql_connection->connect_error);
+    // } else {
+    //   $sql = "SELECT * FROM exercises;";
+    //   $results = $mysql_connection->query($sql);
+    //   return $results;
+    // }
+    $conn = pg_connect("dbname=fit_builder");
+    $query = file_get_contents(__DIR__ . '/../database/sql/exercises/find.sql');
+    $result = pg_query($conn, $query);
+    $exercises = array();
+    $current_exercise = null;
+    while($data = pg_fetch_object($result)){
+      // if($current_exercise === null){
+        $current_exercise = new Exercise(intval($data->id), $data->title, intval($data->intensity), $data->focus, $data->description, $data->image);
+        $exercises[] = $current_exercise;
+      // }
     }
+    return $exercises;
+  }
 
-    static public function find(){
-         //connect just once, not for every create/find
-        $servername = 'localhost';
-        $username = 'root';
-        $password = 'root';
-        $dbname = 'fit_builder';
-
-        $mysql_connection = new mysqli($servername, $username, $password, $dbname);
-
-        if($mysql_connection->connect_error){
-            $mysql_connection->close();
-            die('Connection Failed: ' . $mysql_connection->connect_error);
-        } else {
-            $sql = "SELECT * FROM exercises;";
-            $results = $mysql_connection->query($sql);
-            return $results;
-        }
-    }
+//   static public function create($title, $intensity, $focus, $description, $image){
+//     //connect just once, not for every create/find
+//     //put these in a properties file that gets ignored from git
+//   //   $servername = 'localhost';
+//   //   $username = 'root';
+//   //   $password = 'root';
+//   //   $dbname = 'fit_builder';
+//   //
+//   //   $mysql_connection = new mysqli($servername, $username, $password, $dbname);
+//   //
+//   //   if($mysql_connection->connect_error){
+//   //     die('Connection Failed: ' . $mysql_connection->connect_error);
+//   //   } else {
+//   //     $sql = "INSERT INTO exercises (title, intensity, focus, description, image) VALUES ('".$title."', '".$intensity."', '".$focus."', '".$description."', '".$image."');";
+//   //     $mysql_connection->query($sql);
+//   //   }
+//   //   $mysql_connection->close();
+//   // }
 }
 
 
