a9d5695d3b35ad1a8f2810bf921f2a013328493d
Erin McMackin <mcmackinerin@gmail.com>
2018-05-05 13:18:14 -0400

    angular app connected

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php 'hi' ?>
+<!DOCTYPE html>
+<html lang="en" ng-app="FitBuilder">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="app/assets/styles/style.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+    <script src="app/app.js"></script>
+    <title>Fit Builder</title>
+  </head>
+  <body>
+    <h1>Hi {{2+2}}</h1>
+  </body>
+</html>
