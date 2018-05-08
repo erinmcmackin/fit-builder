081f8b65463ac055d2630bc8095a8889af0e15f2
Erin McMackin <mcmackinerin@gmail.com>
2018-05-08 14:30:25 -0400

    show php working in Postman

--- controllers/exercise.php
+++ controllers/exercise.php
@@ -6,25 +6,27 @@ include_once __DIR__ . '/../models/exercise.php';
 
 // die('test - line 4');
 if($_REQUEST['action'] === 'index'){
-    echo json_encode(Exercises::find());
+  echo json_encode(Exercises::find());
 } else if ($_REQUEST['action'] === 'create'){
-    $requestBody = file_get_contents('php://input');
-    $body = json_decode($requestBody);
+  $requestBody = file_get_contents('php://input');
+  $body = json_decode($requestBody);
 
-    $newExercise = new Exercise(null, $body->title, $body->intensity, $body->focus, $body->description, $body->image);
+  $newExercise = new Exercise(null, $body->title, $body->intensity, $body->focus, $body->description, $body->image);
 
-    $allExercises = Exercises::create($newExercise);
+  $allExercises = Exercises::create($newExercise);
 
-    echo json_encode($allExercises);
+  echo json_encode($allExercises);
 } else if ($_REQUEST['action'] === 'delete'){
-    $allExercises = Exercises::delete($_REQUEST['id']);
-    echo json_encode($allExercises);
+  $allExercises = Exercises::delete($_REQUEST['id']);
+  echo json_encode($allExercises);
 } else if ($_REQUEST['action'] === 'update'){
-    $requestBody = file_get_contents('php://input');
-    $body = json_decode($requestBody);
-    $updatedExercise = new Exercise(null, $body->title, $body->intensity, $body->focus, $body->description, $body->image);
-    $allExercises = Exercises::update($_REQUEST['id'], $updatedExercise);
+  $requestBody = file_get_contents('php://input');
+  $body = json_decode($requestBody);
+  $updatedExercise = new Exercise(null, $body->title, $body->intensity, $body->focus, $body->description, $body->image);
+  $allExercises = Exercises::update($_REQUEST['id'], $updatedExercise);
 
-    echo json_encode($allExercises);
+  echo json_encode($allExercises);
+} else if ($_REQUEST['action'] === 'new'){
+  echo json_encode(Exercises::findShow($_REQUEST['id']));
 }
 ?>
